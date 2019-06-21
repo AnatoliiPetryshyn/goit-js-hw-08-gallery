6db1cc4ecd044310a91f61302fdb35cc9282871e
Anatolii Petryshyn <a.petryshyn777@gmail.com>
2019-06-21 21:12:20 +0300

    hw-08-gallery

---
+++ src/js/main.js
@@ -0,0 +1,79 @@
+'use strict';
+import galleryItems from './gallery-items.js';
+
+const refs = {
+  ul: document.querySelector('.gallery'),
+  div: document.querySelector('.lightbox'),
+  modalImg: document.querySelector('.lightbox__image'),
+  lightbox: document.querySelector('.lightbox'),
+  closeModalBtn: document.querySelector('button[data-action="close-lightbox"]'),
+  lightboxContent: document.querySelector('.lightbox__content'),
+};
+
+// создаем разметку галереи
+const galleryMarkup = galleryItems.reduce((acc, item) => {
+  const itemMarkup = `<li class="gallery__item">
+  <a
+    class="gallery__link"
+    href="${item.original}"
+  >
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+      data-source="${item.original}"
+      alt="${item.description}"
+    />
+
+    <span class="gallery__icon">
+      <i class="material-icons">zoom_out_map</i>
+    </span>
+  </a>
+  </li>`;
+
+  acc += itemMarkup;
+
+  return acc;
+}, '');
+
+refs.ul.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// отображаем изображение оригинального размера
+refs.ul.addEventListener('click', e => {
+  e.preventDefault();
+  if (e.target === e.currentTarget) {
+    return;
+  }
+  const imgUrl = e.target.dataset.source;
+  const imgAlt = e.target.getAttribute('alt');
+
+  refs.modalImg.src = imgUrl;
+  refs.modalImg.alt = imgAlt;
+
+  refs.div.classList.add('is-open');
+  window.addEventListener('keydown', handleKeyPress);
+});
+
+// закрываем модальное окно
+refs.closeModalBtn.addEventListener('click', closeModal);
+refs.lightboxContent.addEventListener('click', handleOverlayClick);
+
+function closeModal() {
+  refs.lightbox.classList.remove('is-open');
+  window.removeEventListener('keydown', handleKeyPress);
+}
+
+function handleOverlayClick(event) {
+  if (event.target !== event.currentTarget) {
+    return;
+  }
+
+  closeModal();
+}
+
+function handleKeyPress(event) {
+  if (event.code !== 'Escape') {
+    return;
+  }
+
+  closeModal();
+}
